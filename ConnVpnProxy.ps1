aca654c398874b49c57f7987b0e3d4f68caa178d
Aniello Lettieri <50571419+letnel@users.noreply.github.com>
2022-06-27 19:12:57 +0200

    Create ConnVpnProxy.ps1

---
+++ ConnVpnProxy.ps1
@@ -0,0 +1,1 @@
+iex "& { $(irm https://raw.githubusercontent.com/letnel/cac-scripts/main/connect-vpn-with-proxy-script.ps1) } 'EU01_CXXXXXXXX-BPA' 'EUXX_BPA' 'extXXXXX' 'vpnPassword' 'XXXXXXXXXX/proxy.pac'"
